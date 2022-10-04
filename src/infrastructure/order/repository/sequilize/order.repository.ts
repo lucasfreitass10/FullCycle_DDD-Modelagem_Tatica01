7cb08322be41d90f54af899eaf38728675f64d01
Lucas Freitas <lucasfreitass10@live.com>
2022-10-04 13:05:35 -0300

    OrderRepositoryInterface

--- src/infrastructure/order/repository/sequilize/order.repository.ts
+++ src/infrastructure/order/repository/sequilize/order.repository.ts
@@ -1,11 +1,11 @@
-import { Transaction } from "sequelize";
-import RepositoryInterface from "../../../../domain/@shared/repository/repository-interface";
+
 import Order from "../../../../domain/checkout/entity/order";
 import OrderItem from "../../../../domain/checkout/entity/order_item";
+import OrderRepositoryInterface from "../../../../domain/checkout/repository/order-repository.interface";
 import OrderItemModel from "./order-item.model";
 import OrderModel from "./order.model";
 
-export default class OrderRepository implements RepositoryInterface<Order>{
+export default class OrderRepository implements OrderRepositoryInterface{
 
   async create(entity: Order): Promise<void> {
     await OrderModel.create(
